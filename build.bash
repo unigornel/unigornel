82135db6b59704bb19be2886f6ef8ba0ef24a36a
Henri Verroken <henriverroken@gmail.com>
2016-03-31 17:07:02 +0200

    Add command to build full application

--- build.bash
+++ build.bash
@@ -7,7 +7,7 @@ function usage {
     echo "       build.bash go --gobootstrap path"
     echo "       build.bash minios --goarchive app/app.a --goinclude app/"
     echo "       build.bash compile --app path/to/app/dir/ [-o path/to/app.a] [-a] [-x]"
-    echo "       build.bash app --app path/to/app/dir/ [-a] [-x]"
+    echo "       build.bash app --app path/to/app/dir/ [-o minios] [-a] [-x]"
 }
 
 
@@ -20,6 +20,7 @@ O_FLAG=
 A_FLAG=n
 X_FLAG=n
 
+SCRIPT="$0"
 CMD="$1"
 shift
 
@@ -66,7 +67,7 @@ if [ "$CMD" = go ]; then
         error "missing --gobootstrap flag"
     fi
 
-# Build application
+# Compile Go
 elif [ "$CMD" = "compile" ]; then
     if [ -n "$APP_DIR" ]; then
         [ -z "$GOPATH" ] && error "cannot build app: GOPATH not set"
@@ -74,7 +75,7 @@ elif [ "$CMD" = "compile" ]; then
         BUILD_DIR=build-$$
 
         # Copy header files
-        echo "Building application in ./$BUILD_DIR"
+        echo "Compiling Go in ./$BUILD_DIR"
         do_cmd mkdir "$BUILD_DIR"
         do_cmd cp Makefile.app "$BUILD_DIR"/Makefile
         pushd "$BUILD_DIR"
@@ -101,6 +102,7 @@ elif [ "$CMD" = "compile" ]; then
                GOOS=netbsd \
                GOARCH=amd64 \
                "$goroot"/bin/go build -buildmode=c-archive $opts -o "$out"
+        do_cmd objcopy --globalize-symbol=_rt0_amd64_netbsd_lib "$out"
         popd
 
         do_cmd rm -r "$BUILD_DIR"
@@ -121,6 +123,37 @@ elif [ "$CMD" = minios ]; then
 
     else
         error "missing --goarchive and/or --goinclude flag"
+    fi
+
+# Build full application
+elif [ "$CMD" = app ]; then
+    if [ -n "$APP_DIR" ]; then
+        BUILD_DIR=build-$$
+        echo "Building application in ./$BUILD_DIR"
+        do_cmd mkdir "$BUILD_DIR"
+
+        # Compile Go
+        echo "[+] Compiling Go code"
+        export GOPATH
+        opts=
+        [ "$X_FLAG" = y ] && opts="$opts -x"
+        [ "$A_FLAG" = y ] && opts="$opts -a"
+        do_cmd "$SCRIPT" compile --app "$APP_DIR" $opts -o "$BUILD_DIR"/app.a
+
+        # Compile Mini-OS
+        echo "[+] Compiling Mini-OS"
+        do_cmd "$SCRIPT" minios --goarchive "$BUILD_DIR"/app.a --goinclude "$BUILD_DIR"
+        do_cmd rm -r "$BUILD_DIR"
+
+        # Copy minios
+        if [ -n "$O_FLAG" ]; then
+            do_cmd cp minios/mini-os "$O_FLAG"
+            echo "[+] Unikernel is in $O_FLAG"
+        else
+            echo "[+] Unikernel is in minios/mini-os"
+        fi
+    else
+        error "missing --app flag"
     fi
 
 # Unknown command
