5ed17f9df3050123efe240eb1411fbc434fc2b5c
Henri Verroken <henriverroken@gmail.com>
2016-03-31 16:50:47 +0200

    Use subcommands in build script

--- build.bash
+++ build.bash
@@ -2,8 +2,16 @@
 
 set -e
 
+function usage {
+    echo "usage: build.bash help|-h|--help"
+    echo "       build.bash go --gobootstrap path"
+    echo "       build.bash minios --goarchive app/app.a --goinclude app/"
+    echo "       build.bash compile --app path/to/app/dir/ [-o path/to/app.a] [-a] [-x]"
+    echo "       build.bash app --app path/to/app/dir/ [-a] [-x]"
+}
+
+
 HELP=n
-DID_ANYTHING=n
 [ -z "$GOROOT_BOOTSTRAP" ] && GOROOT_BOOTSTRAP=
 MINIOS_GOARCHIVE=
 MINIOS_GOINCLUDE=
@@ -11,6 +19,9 @@ APP_DIR=
 O_FLAG=
 A_FLAG=n
 X_FLAG=n
+
+CMD="$1"
+shift
 
 TEMP=`getopt -o ho:ax --long help,gobootstrap:,goarchive:,goinclude:,app: -n build.bash -- "$@"`
 eval set -- "$TEMP"
@@ -39,89 +50,81 @@ function error {
 }
 
 # Show help
-function usage {
-    echo "usage: build.bash -h|--help"
-    echo "       build.bash --gobootstrap path"
-    echo "       build.bash --goarchive app/app.a --goinclude app/"
-    echo "       build.bash --app path/to/app/dir/ [-o path/to/app.a]"
-    echo "                  [-a] [-x]"
-}
-
-if [ "$HELP" = y ]; then
+if [ "$HELP" = y -o "$CMD" = help ]; then
     usage
     exit 0
 fi
 
 # Build Go
-if [ -n "$GOROOT_BOOTSTRAP" ]; then
-    DID_ANYTHING=y
-
-    pushd go/src
-    echo "Building Go in $PWD"
-    do_cmd GOROOT_BOOTSTRAP="$GOROOT_BOOTSTRAP" GOOS=netbsd GOARCH=amd64 ./make.bash
-    popd
-fi
+if [ "$CMD" = go ]; then
+    if [ -n "$GOROOT_BOOTSTRAP" ]; then
+        pushd go/src
+        echo "Building Go in $PWD"
+        do_cmd GOROOT_BOOTSTRAP="$GOROOT_BOOTSTRAP" GOOS=netbsd GOARCH=amd64 ./make.bash
+        popd
+    else
+        error "missing --gobootstrap flag"
+    fi
 
 # Build application
-if [ -n "$APP_DIR" ]; then
-    [ -z "$GOPATH" ] && error "cannot build app: GOPATH not set"
+elif [ "$CMD" = "compile" ]; then
+    if [ -n "$APP_DIR" ]; then
+        [ -z "$GOPATH" ] && error "cannot build app: GOPATH not set"
 
-    DID_ANYTHING=y
+        BUILD_DIR=build-$$
 
-    BUILD_DIR=build-$$
+        # Copy header files
+        echo "Building application in ./$BUILD_DIR"
+        do_cmd mkdir "$BUILD_DIR"
+        do_cmd cp Makefile.app "$BUILD_DIR"/Makefile
+        pushd "$BUILD_DIR"
+        echo "[+] Copying header files"
+        do_cmd MINIOS_ROOT=../minios make include/mini-os
+        popd
 
-    # Copy header files
-    echo "Building application in ./$BUILD_DIR"
-    do_cmd mkdir "$BUILD_DIR"
-    do_cmd cp Makefile.app "$BUILD_DIR"/Makefile
-    pushd "$BUILD_DIR"
-    echo "[+] Copying header files"
-    do_cmd MINIOS_ROOT=../minios make include/mini-os
-    popd
+        # Compile application
+        echo "[+] Compiling Go application in $APP_DIR"
+        [ -z "$O_FLAG" ] && out="$BUILD_DIR"/a.out || out="$O_FLAG"
+        out="$(realpath "$out")"
+        include="$(realpath "$BUILD_DIR"/include)"
+        goroot="$(realpath "../go")"
 
-    # Compile application
-    echo "[+] Compiling Go application in $APP_DIR"
-    [ -z "$O_FLAG" ] && out="$BUILD_DIR"/a.out || out="$O_FLAG"
-    out="$(realpath "$out")"
-    include="$(realpath "$BUILD_DIR"/include)"
-    goroot="$(realpath "../go")"
+        opts=
+        [ "$X_FLAG" = y ] && opts="$OPTS -x"
+        [ "$A_FLAG" = y ] && opts="$OPTS -a"
 
-    opts=
-    [ "$X_FLAG" = y ] && opts="$OPTS -x"
-    [ "$A_FLAG" = y ] && opts="$OPTS -a"
+        pushd "$APP_DIR"
+        export GOPATH
+        do_cmd GOROOT="$goroot" \
+               CGO_ENABLED=1 \
+               CGO_CFLAGS=-I"$include" \
+               GOOS=netbsd \
+               GOARCH=amd64 \
+               "$goroot"/bin/go build -buildmode=c-archive $opts -o "$out"
+        popd
 
-    pushd "$APP_DIR"
-    export GOPATH
-    do_cmd GOROOT="$goroot" \
-           CGO_ENABLED=1 \
-           CGO_CFLAGS=-I"$include" \
-           GOOS=netbsd \
-           GOARCH=amd64 \
-           "$goroot"/bin/go build -buildmode=c-archive $opts -o "$out"
-    popd
-
-    do_cmd rm -r "$BUILD_DIR"
-fi
+        do_cmd rm -r "$BUILD_DIR"
+    else
+        error "missing --app flag"
+    fi
 
 # Build Mini-OS
-if [ -n "$MINIOS_GOARCHIVE" -a -n "$MINIOS_GOINCLUDE" ]; then
-    DID_ANYTHING=y
+elif [ "$CMD" = minios ]; then
+    if [ -n "$MINIOS_GOARCHIVE" -a -n "$MINIOS_GOINCLUDE" ]; then
+        archive="$(realpath "$MINIOS_GOARCHIVE")"
+        include="$(realpath "$MINIOS_GOINCLUDE")"
 
-    archive="$(realpath "$MINIOS_GOARCHIVE")"
-    include="$(realpath "$MINIOS_GOINCLUDE")"
+        pushd minios
+        echo "Building Mini-OS in $PWD"
+        do_cmd make GOARCHIVE="$archive" GOINCLUDE="$include"
+        popd
 
-    pushd minios
-    echo "Building Mini-OS in $PWD"
-    do_cmd make GOARCHIVE="$archive" GOINCLUDE="$include"
-    popd
+    else
+        error "missing --goarchive and/or --goinclude flag"
+    fi
 
-elif [ -n "$MINIOS_GOARCHIVE" -o -n "$MINIOS_GOINCLUDE" ]; then
-    usage >&2
-    exit 1
-fi
-
-# Make sure something is done
-if [ "$DID_ANYTHING" = n ]; then
-    usage >&2
-    exit 1
+# Unknown command
+else
+    usage
+    error "unknown command: $CMD"
 fi
