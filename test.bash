0f844b0228b49051aceca90a996579bb8eb055de
Henri Verroken <henriverroken@gmail.com>
2016-04-01 18:36:41 +0200

    Run integration tests in test.bash

--- test.bash
+++ test.bash
@@ -12,21 +12,23 @@ function itime {
 }
 
 BUILD_GO=y
+INTEGRATION=y
 SHOW_HELP=n
 
-TEMP=`getopt -o h --long help,no-go -n test.bash -- "$@"`
+TEMP=`getopt -o h --long help,no-go,no-integration -n test.bash -- "$@"`
 eval set -- "$TEMP"
 while true; do
     case "$1" in
         -h|--help)      SHOW_HELP=y         ; shift   ;;
         --no-go)        BUILD_GO=n          ; shift   ;;
+        --no-integration) INTEGRATION=n     ; shift   ;;
         --)             shift               ; break   ;;
     esac
 done
 
 if [ "$SHOW_HELP" = y ]; then
     echo "usage: test.bash -h|--help"
-    echo "       test.bash [--no-go]"
+    echo "       test.bash [--no-go] [--no-integration]"
     exit 0
 fi
 
@@ -42,5 +44,10 @@ if [ "$BUILD_GO" = y ]; then
     do_cmd itime -p ./build.bash go
 fi
 
-# Build test application
-GOPATH=$PWD/integration_tests/hello_world/go do_cmd itime -p ./build.bash app -a -x --app integration_tests/hello_world/go/src/helloworld/
+# Run integration tests
+if [ "$INTEGRATION" = y ]; then
+    unigornel_root="$PWD"
+    pushd integration_tests
+    UNIGORNEL_ROOT="$unigornel_root" do_cmd itime -p python3 test.py
+    popd
+fi
