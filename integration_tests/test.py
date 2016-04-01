a06c04fc66aa58f2fc647180a185e33a4bc8f5da
Henri Verroken <henriverroken@gmail.com>
2016-04-01 20:50:41 +0200

    Generate JUnit XML

--- integration_tests/test.py
+++ integration_tests/test.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import time
 import logging
+
+from junit_xml import TestCase, TestSuite
 
 SILENT = False
 
@@ -37,15 +40,29 @@ class IntegrationTest(object):
         return list(tests)
 
     def run(self, unigornel_root):
-        self.build(unigornel_root)
+        steps = [
+            lambda: self.build(unigornel_root)
+        ]
+        cases = []
+        for s in steps:
+            c = s()
+            cases.append(c)
+            if c.is_failure():
+                break
+
+        return TestSuite(self.name, cases)
 
     def build(self, unigornel_root):
-        log("Building '{0}': package {1}".format(self.name, self.package))
+        def f():
+            log("Building '{0}': package {1}".format(self.name, self.package))
 
-        gopath = os.path.join(self.path, 'go')
-        app_path = os.path.join(self.path, 'go', 'src', self.package)
-        app = UnigornelApp(app_path, gopath, unigornel_root)
-        app.build()
+            gopath = os.path.join(self.path, 'go')
+            app_path = os.path.join(self.path, 'go', 'src', self.package)
+            app = UnigornelApp(app_path, gopath, unigornel_root)
+            output = app.build()
+            log(output)
+            return output, None
+        return build_test_case('build', self.name, f)
 
     def execute(self):
         pass
@@ -62,7 +79,7 @@ class UnigornelApp(object):
         self.unigornel_root = unigornel_root
 
     def build(self, out=None, build_all=True, verbose=True):
-        from subprocess import Popen, PIPE
+        from subprocess import Popen, PIPE, STDOUT
 
         argv = [self.BUILD_CMD, 'app', '--app', self.path]
         if out is not None: argv += ['-o', out]
@@ -73,10 +90,16 @@ class UnigornelApp(object):
         env['GOPATH'] = self.gopath
 
         with pushd(self.unigornel_root):
-            with Popen(argv, env=env):
-                pass
+            log('[+]', ' '.join(argv))
+            with Popen(argv, env=env, stdout=PIPE, stderr=STDOUT) as proc:
+                b = proc.stdout.read()
+                try:
+                    stdout = b.decode('utf-8')
+                except UnicodeDecodeError:
+                    stdout = str(b)
+                return stdout
 
-def main(unigornel_root, tests):
+def main(unigornel_root, tests, **kwargs):
     if tests is not None:
         try:
             all_tests = list(map(IntegrationTest.load, tests))
@@ -86,17 +109,22 @@ def main(unigornel_root, tests):
         all_tests = IntegrationTest.discover()
 
     n = len(all_tests)
+    test_suites = []
     log('Running {0} test(s)'.format(n))
     for i, test in enumerate(all_tests):
         log("----------")
         log("Running test '{0}' ({1}/{2})".format(test.name, i+1, n))
-        test.run(unigornel_root)
+        ts = test.run(unigornel_root)
+        test_suites.append(ts)
+    return test_suites
+
 
 def parse(argv, envs):
     from argparse import ArgumentParser
     parser = ArgumentParser(description='Execute the integration tests')
     parser.add_argument('tests', metavar='test', type=str, nargs='*', help='only run specified tests')
     parser.add_argument('-s', dest='silent', action='store_true', help='silent mode')
+    parser.add_argument('--junit', type=str, help='write junit xml to specified file')
 
     args = parser.parse_args(argv)
     try:
@@ -111,6 +139,7 @@ def parse(argv, envs):
     return {
         'unigornel_root' : unigornel_root,
         'tests' : args.tests if len(args.tests) != 0 else None,
+        'junit' : args.junit,
     }
 
 def log(*args, **kwargs):
@@ -136,6 +165,27 @@ def pushd(dirname):
             chdir(self.original_dir)
     return PushdContext(dirname)
 
+def build_test_case(name, classname, f):
+    from traceback import format_exc
+    start = time.monotonic()
+    try:
+        output, failure = f()
+    except Exception as e:
+        output = format_exc(e)
+        failure = "An unexpected exception occurred."
+
+    elapsed = time.monotonic() - start
+    tc = TestCase(name, classname, elapsed, output)
+    if failure is not None:
+        tc.add_failure_info(failure)
+    return tc
+
 if __name__ == "__main__":
     args = parse(sys.argv[1:], os.environ)
-    main(**args)
+    suites = main(**args)
+
+    junit = args['junit']
+    if junit:
+        log('Writing', len(suites), ' test suites to', junit)
+        with open(junit, 'w') as f:
+            TestSuite.to_file(f, suites)
