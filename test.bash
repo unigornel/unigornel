97f86741a14ee4b51f7aeedbd890db229cde6ee7
Henri Verroken <henriverroken@gmail.com>
2016-06-17 14:24:48 +0200

    Setup a GOPATH for testing when necessary

--- test.bash
+++ test.bash
@@ -15,8 +15,9 @@ BUILD_GO=y
 INTEGRATION=y
 FAST=n
 SHOW_HELP=n
+SETUP_GOPATH=n
 
-TEMP=`getopt -o h --long help,no-go,no-integration,fast -n test.bash -- "$@"`
+TEMP=`getopt -o h --long help,no-go,no-integration,fast,setup-gopath -n test.bash -- "$@"`
 eval set -- "$TEMP"
 while true; do
     case "$1" in
@@ -24,13 +25,14 @@ while true; do
         --no-go)        BUILD_GO=n          ; shift   ;;
         --no-integration) INTEGRATION=n     ; shift   ;;
         --fast)         FAST=y              ; shift   ;;
+        --setup-gopath) SETUP_GOPATH=y      ; shift   ;;
         --)             shift               ; break   ;;
     esac
 done
 
 if [ "$SHOW_HELP" = y ]; then
     echo "usage: test.bash -h|--help"
-    echo "       test.bash [--no-go] [--no-integration] [--fast]"
+    echo "       test.bash [--no-go] [--no-integration] [--fast] [--setup-gopath]"
     exit 0
 fi
 
@@ -38,6 +40,15 @@ function do_cmd {
     echo "[+] $@"
     eval "$@"
 }
+
+PROJECT_ROOT="$PWD"
+if [ "$SETUP_GOPATH" = y ]; then
+    do_cmd mkdir -p gopath/src/github.ugent.be
+    do_cmd rm -f gopath/src/github.ugent.be/unigornel
+    do_cmd ln -s ../../.. gopath/src/github.ugent.be/unigornel
+    export GOPATH="$PWD/gopath"
+    do_cmd cd gopath/src/github.ugent.be/unigornel
+fi
 
 # Build Go
 if [ "$BUILD_GO" = y ]; then
