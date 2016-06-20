693d5f0f0e622846552d989bd2650b5724cd2b4a
Henri Verroken <henriverroken@gmail.com>
2016-06-20 14:12:33 +0200

    Add support for faster builds

--- jenkins.bash
+++ jenkins.bash
@@ -2,4 +2,9 @@
 
 set -e
 
-taskset -c "${EXECUTOR_NUMBER}" ./test.bash --setup-gopath
+opts=
+if [ "$1" = "--fast" ]; then
+    opts=--fast
+fi
+
+taskset -c "${EXECUTOR_NUMBER}" ./test.bash --setup-gopath $opts
