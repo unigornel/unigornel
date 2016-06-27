80f2009beda62ed1376957b3263c821f42c8bd84
Henri Verroken <henriverroken@gmail.com>
2016-06-28 01:42:46 +0200

    Use new network layer structure

--- integration_tests/tests/network/ping/main.go
+++ integration_tests/tests/network/ping/main.go
@@ -11,20 +11,15 @@ func main() {}
 
 //export Main
 func Main(unused int) {
-	ip := [4]byte{10, 0, 100, 2}
+	sourceIP := [4]byte{10, 0, 100, 2}
 
 	nic := ethernet.NewNIC()
-	ethDemux := ethernet.NewDemux(nic.Receive(), ethernet.DemuxLog)
+	eth := ethernet.NewLayer(nic)
+	arp := ipv4.NewARP(nic.GetMAC(), sourceIP, eth)
+	ip := ipv4.NewLayer(sourceIP, arp, eth)
+	icmp.NewLayer(ip)
 
-	arp := ipv4.NewARP(nic.GetMAC(), ip, nic.Send())
-	arp.Bind(ethDemux)
-
-	ipl := ipv4.NewLayer([4]byte{10, 0, 100, 2}, arp, nic.Send())
-	ipDemux := ipv4.NewDemux(ipl.Receive(), ipv4.DemuxLog)
-	ipl.Bind(ethDemux)
-
-	icmpl := icmp.NewLayer(ipl.Send())
-	icmpl.Bind(ipDemux)
+	nic.Start()
 
 	m := make(chan int)
 	m <- 0
