9c23a1357a540caac2420909a1102a2596c189c8
Henri Verroken <henriverroken@gmail.com>
2016-06-28 12:26:03 +0200

    Fetch Pull Request branches in submodules

--- jenkins.bash
+++ jenkins.bash
@@ -2,6 +2,17 @@
 
 set -e
 
+PR_REFSPEC='+refs/pull/*:refs/remotes/origin/pr/*'
+
+add_pr_refs() {
+    if ! git config --get-all remote.origin.fetch | grep -F "$PR_REFSPEC" > /dev/null; then
+        git config --add remote.origin.fetch "$PR_REFSPEC"
+    fi
+}
+
+pushd go && add_pr_refs && popd
+pushd minios && add_pr_refs && popd
+
 opts=
 if [ "$1" = "--fast" ]; then
     opts=--fast
